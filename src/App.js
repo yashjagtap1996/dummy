45832c6c89d95c39529671debcff84fd3ec8245e
Yash Suresh Jagtap <91862550+yashjagtap1996@users.noreply.github.com>
2022-05-01 08:21:08 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import CompA from './Component/CompA';
+import CompC from './Component/CompC';
 
 const App = () => {
   return (
     <>
       <h1>Hello world</h1>
       <CompA />
+      <CompC />
     </>
   )
 }
---
+++ src/Component/CompC.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CompC = () => {
+  return (
+    <>
+        <h1>This is CompC.js</h1>
+    </>
+  )
+}
+
+export default CompC
